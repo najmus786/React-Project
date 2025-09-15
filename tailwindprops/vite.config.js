6960c0f4ef8f753429a824f6ef1698645e754b1a
Najmus Saquib <najmussaquib64@gmail.com>
2025-09-15 20:06:59 +0530

    bgchanger

---
+++ tailwindprops/vite.config.js
@@ -0,0 +1,11 @@
+
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),tailwindcss(),],
+})
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+
